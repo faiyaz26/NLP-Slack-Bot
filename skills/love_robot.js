ca2494158c5c7f2bfaa2eba3808da96d4d7fd159
EC2 Default User <ec2-user@ip-172-30-83-234.us-west-2.compute.internal>
2016-09-19 08:47:47 +0000

    added new skill

---
+++ skills/love_robot.js
@@ -0,0 +1,8 @@
+var answer = 'I am Bot! A bot doesn`t have feelings. So I don't understand what is love.';
+module.exports = function(skill, info, bot, message) {
+
+	                    	console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
+
+	                    	bot.reply(message, answer);
+
+	                	};
