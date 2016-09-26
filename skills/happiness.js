981b955bd300c4fc95a37f0f6aea29c05f3e6f46
Ahmad Faiyaz <faiyaz26@gmail.com>
2016-09-26 16:07:32 +0600

    fixed js issue

--- skills/happiness.js
+++ skills/happiness.js
@@ -1,9 +1,8 @@
-var answer = 'William E. Gladstone said-\n
->Be happy with what you have and are, be generous with both, and you won\'t have to hunt for happiness.';
+var answer = 'William E. Gladstone said-\n' + '>Be happy with what you have and are, be generous with both, and you won\'t have to hunt for happiness.';
 module.exports = function(skill, info, bot, message) {
 
-                            console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
+    console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
 
-                            bot.reply(message, answer);
+    bot.reply(message, answer);
 
-                        };
+};
