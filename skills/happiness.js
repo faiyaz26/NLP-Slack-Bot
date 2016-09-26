8d023cd57ffd6327e630e523a8dddacad51df655
EC2 Default User <ec2-user@ip-172-30-83-234.us-west-2.compute.internal>
2016-09-26 10:00:52 +0000

    added happiness

---
+++ skills/happiness.js
@@ -0,0 +1,9 @@
+var answer = 'William E. Gladstone said-\n
+>Be happy with what you have and are, be generous with both, and you won\'t have to hunt for happiness.';
+module.exports = function(skill, info, bot, message) {
+
+                            console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
+
+                            bot.reply(message, answer);
+
+                        };
